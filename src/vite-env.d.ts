9b9cf3d97e829bc378b091d33f32e74c3ef2a348
Jinsan T J <hellojinsantj@gmail.com>
2024-05-13 01:06:58 +0530

    added type definition for vite env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,22 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    /**
+     * The current environment.
+     */
+    readonly VITE_ENV: 'DEVELOPMENT' | 'PRODUCTION'
+
+    /**
+     * The base path
+     */
+    readonly VITE_BASE_PATH: string
+
+    /**
+     * The Json Placeholder API URL
+     */
+    readonly VITE_API_URL: string
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
