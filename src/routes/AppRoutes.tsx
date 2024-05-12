b34dcaad060aa8c72131b0010cf5a9630458dfb0
Jinsan T J <hellojinsantj@gmail.com>
2024-05-13 01:07:10 +0530

    added routes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,6 +2,7 @@ import type { FC } from 'react'
 import { Route, Routes } from 'react-router-dom'
 
 import { BaseLayout } from '../layouts/views'
+import { PostItem, PostList } from '../features/posts'
 
 /**
  * The application routes.
@@ -11,7 +12,10 @@ import { BaseLayout } from '../layouts/views'
 export const AppRoutes: FC = () => {
     return (
         <Routes>
-            <Route path='/' element={<BaseLayout />} />
+            <Route path='/' element={<BaseLayout />}>
+                <Route path='/' element={<PostList />} />
+                <Route path='/posts/:postId' element={<PostItem />} />
+            </Route>
         </Routes>
     )
 }
